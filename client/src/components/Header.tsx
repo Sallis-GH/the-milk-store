dbbed8c9763c4b3cda008267287622f0dbfeb81c
Sallis-GH <74314359+Sallis-GH@users.noreply.github.com>
2023-01-23 17:42:18 +0100

    header complete

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-center text-[#da88a4] uppercase tracking-widest font-semibold text-2xl py-2 bg-[#ffffff]">The Milk Store</h1>
+    </header>
+  )
+}
+
+export default Header
